56929ba0e4cec5138cf60b33f06e6dd7194d1762
Sushree-01 <subhashree.payal2000@gmail.com>
2023-06-19 15:48:22 +0530

    completed

--- trading_hub/src/Pages/Login.jsx
+++ trading_hub/src/Pages/Login.jsx
@@ -1,69 +1,52 @@
-import {
-  Flex,
-  Box,
-  FormControl,
-  FormLabel,
-  Input,
-  Checkbox,
-  Stack,
-  Link,
-  Button,
-  Heading,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
+import { useState, useContext } from 'react';
+import { Flex, Box, FormControl, FormLabel, Input, Checkbox, Stack, Link, Button, Heading, Text, useColorModeValue } from '@chakra-ui/react';
 import axios from 'axios';
-import { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Context/AuthContext';
 
+const users = {
+  email: '',
+  password: '',
+};
 
-const users={
-email : "",
-password : ""
-}
-export default function  Login() {
-    const [User, setUser] = useState(users);
-    const {login,token }=useContext(AuthContext);
-        const   navigate =  useNavigate()
-    const handleChange=(e)=>{
-       const { name , value } = e.target; 
-      
-       setUser({...User, [name] : value })
-    }
+export default function Login() {
+  const [User, setUser] = useState(users);
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const handleSubmit=(e)=>{
-      e.preventDefault();
-      
-      handlePost();
-      console.log(User)
-    }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...User, [name]: value });
+  };
 
-    const handlePost=async()=>{
-      try {
-        return axios({
-          method : "post",
-          url : `https://reqres.in/api/login`,
-          data : User
-        }).then((res)=>{ 
-          if(res.data.token){
-            console.log(res.data.token)
-            login(res.data.token)
-            navigate("/")
-          }
-        })
-      } catch (error) {
-        console.log("err")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handlePost();
+    console.log(User);
+  };
+
+  const handlePost = async () => {
+    try {
+      const res = await axios.post('https://reqres.in/api/login', User);
+      if (res.data.token) {
+        console.log(res.data.token);
+        login(res.data.token);
+        navigate('/');
       }
+    } catch (error) {
+      console.log('error');
     }
+  };
 
-const {email, password } = User;
+  const { email, password } = User;
+
   return (
     <Flex
       minH={'100vh'}
       align={'center'}
       justify={'center'}
-      bg={useColorModeValue('gray.50', 'gray.800')}>
+      bg={useColorModeValue('gray.50', 'gray.800')}
+    >
       <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
         <Stack align={'center'}>
           <Heading fontSize={'4xl'}>Sign in to your account</Heading>
@@ -71,54 +54,49 @@ const {email, password } = User;
             to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
           </Text>
         </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-            <form onSubmit={handleSubmit}> 
-          <Stack spacing={4}>
-            {/* email  */}
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" 
-              name="email"
-              value={email}
-              onChange={handleChange}
-              />
-            </FormControl>
+        <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8}>
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={4}>
+              {/* email */}
+              <FormControl id="email">
+                <FormLabel>Email address</FormLabel>
+                <Input
+                  type="email"
+                  name="email"
+                  value={email}
+                  onChange={handleChange}
+                />
+              </FormControl>
 
-          {/* password */}
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" 
-              name="password"
-              value={password}
-              onChange={handleChange}               
-              />
-            </FormControl>
+              {/* password */}
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  type="password"
+                  name="password"
+                  value={password}
+                  onChange={handleChange}
+                />
+              </FormControl>
 
-            <Stack spacing={10}>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}>
-                <Checkbox>Remember me</Checkbox>
-                <Link color={'blue.400'}>Forgot password?</Link>
-              </Stack>
-              <Button
-                bg={'blue.400'}
-                color={'white'}
-                _hover={{
-                  bg: 'blue.500',
-                }}
-                type="submit"
+              <Stack spacing={10}>
+                <Stack direction={{ base: 'column', sm: 'row' }} align={'start'} justify={'space-between'}>
+                  <Checkbox>Remember me</Checkbox>
+                  <Link color={'blue.400'}>Forgot password?</Link>
+                </Stack>
+                <Button
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}
+                  type="submit"
                 >
-                Sign in
-              </Button>
+                  Sign in
+                </Button>
+              </Stack>
             </Stack>
-          </Stack>
-            </form> 
+          </form>
         </Box>
       </Stack>
     </Flex>
